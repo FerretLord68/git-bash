3b3719bfbddfb8586de9fe3b3e4fed2b672accf6
Frederik Olsen <frederikjuulolsen@gmail.com>
2024-04-18 19:29:05 +0200

    new-user eddit

--- new-user.sh
+++ new-user.sh
@@ -6,20 +6,16 @@ read username
 echo "what group does the user belong in?"
 read usergroup
 
-echo "what should the password for the user be?"
-read password
-
 #after getting the information for the new user creation, the user will be createde, with a home dir + givien the correct group.
 
 #creating the user with a home dir
-$(useradd -m $username)
-
-#creating the password for the user
-$(passwd $username)
-$($password)
+$(useradd --create-home $username)
 
 #adding the user to the correct group
 $(adduser $username $usergroup)
+
+#creating tempery password for new user
+$(passwd $username)
 
 #force the new user to create a new login on first login
 $(passwd --expire $username)

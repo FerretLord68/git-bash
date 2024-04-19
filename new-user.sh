9d6d38e5cbf899f48fcee4786a6a892625f33179
Frederik Olsen <frederikjuulolsen@gmail.com>
2024-04-19 09:57:24 +0200

    ...

--- new-user.sh
+++ new-user.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 TEMPPASS=Trythis1
-USERNAME=$USERNAME
-USERGROUP=$USERGROUP
 
 echo "what's the username of the new user?"
 read USERNAME
 
 echo "what group does the user belong in?"
-read USERNAME
+read USERGROUP
 
 #after getting the information for the new user creation, the user will be createde, with a home dir + givien the correct group.
 
 #creating the user with a home dir
-useradd -m -d /home/$USERNAME $USERNAME
+useradd -m $USERNAME
+
+usermod -d /home/$USERNAME $USERNAME
 
 #creating tempery password for new user
 passwd $USERNAME

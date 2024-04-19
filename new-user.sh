d2081479d3aa4aef98951a0e82162d7311a75efa
Frederik Olsen <frederikjuulolsen@gmail.com>
2024-04-19 08:03:50 +0200

    new-user eddit

--- new-user.sh
+++ new-user.sh
@@ -9,13 +9,13 @@ read usergroup
 #after getting the information for the new user creation, the user will be createde, with a home dir + givien the correct group.
 
 #creating the user with a home dir
-$(useradd --create-home $username)
+useradd --create-home $username
 
 #creating tempery password for new user
-$(passwd $username)
+passwd $username
 
 #adding the user to the correct group
-$(adduser $username $usergroup)
+adduser -aG $username $usegroup
 
 #force the new user to create a new login on first login
-$(passwd --expire $username)
+passwd --expire $username
